1c05d35c947ac6ede1806a6db8c149cdca2b276e
Atlchris67 <atlchris@gmail.com>
2019-10-21 09:03:32 -0400

    added-nav-feature
    issues with system.web.mvc checking into

---
+++ src/Feature/Navigation/code/Controllers/NavigationController.cs
@@ -0,0 +1,39 @@
+﻿
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+using System.Web.Mvc;
+//using Helixbase.Feature.Navigation.Mediators;
+//using Helixbase.Foundation.Core.Exceptions;
+
+namespace Helixbase.Feature.Navigation.Controllers
+{
+    public class NavigationController : SitecoreController
+    {
+        //private readonly INavigationMediator _NavigationMediator;
+
+        //public NavigationController(INavigationMediator NavigationMediator)
+        //{
+        //    _NavigationMediator = NavigationMediator;
+        //}
+
+        public ActionResult Navigation()
+        {
+            
+               string navDataSourceId = RenderingContext.Current.Rendering.DataSource;
+            return View("");
+
+            //var mediatorResponse = _NavigationMediator.RequestNavigationViewModel();
+
+            //switch (mediatorResponse.Code)
+            //{
+            //    case MediatorCodes.NavigationResponse.DataSourceError:
+            //    case MediatorCodes.NavigationResponse.ViewModelError:
+            //        return View("~/views/Navigation/Error.cshtml");
+            //    case MediatorCodes.NavigationResponse.Ok:
+            //        return View(mediatorResponse.ViewModel);
+            //    default:
+            //        throw new InvalidMediatorResponseCodeException(mediatorResponse.Code);
+            //}
+        }
+    }
+}
